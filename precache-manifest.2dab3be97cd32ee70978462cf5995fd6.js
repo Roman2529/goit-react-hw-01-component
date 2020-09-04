d451c45818e9903cb01bed5c696685d2b5803463
Roman Demikhov <demikhovroman@gmail.com>
2020-09-04 08:44:29 +0300

    Updates

---
+++ precache-manifest.2dab3be97cd32ee70978462cf5995fd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e1321b515687116eacab63e9f549f2",
+    "url": "/goit-react-hw-01-component/index.html"
+  },
+  {
+    "revision": "1f0bbab6eacd421928de",
+    "url": "/goit-react-hw-01-component/static/css/main.56d74807.chunk.css"
+  },
+  {
+    "revision": "0eaf324306c0ae1a9021",
+    "url": "/goit-react-hw-01-component/static/js/2.a5e06ec9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-component/static/js/2.a5e06ec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f0bbab6eacd421928de",
+    "url": "/goit-react-hw-01-component/static/js/main.3c55f0a8.chunk.js"
+  },
+  {
+    "revision": "78c81943cead774ef2fa",
+    "url": "/goit-react-hw-01-component/static/js/runtime-main.b8e8a918.js"
+  }
+]);
